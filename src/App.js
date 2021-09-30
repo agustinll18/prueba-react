221f71e1af8d1d29641b7d803af4adaa550ae66f
Agustin LLados <agustinllados@gmail.com>
2021-09-30 18:50:28 -0300

    creando el NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenido a la tienda
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="row ">
+			<div class="col-lg-12 col-md-12 col-sm-12"></div>
+			<header>
+				<figcaption> <h1><b> LLadosgaming <small>Copyright &copy; Todos los derechos reservados</small></b></h1>
+				</figcaption>
+				<section class="Menu">
+					<ul>
+						<li><a href="../Home/Home2.html">Home</a>
+						</li>
+						<li><a href="../Tienda/Tienda2.html">Componentes</a>
+						</li>
+						<li><a href="../SobreNosotros2/SobreNosotros2.html">Sobre Nosotros</a>
+						</li>
+						<li><a href="../Registro/Registro2.html">Registro</a>
+							<ul class="Drop">
+								<li> <a href="../Sucursales/Sucursales.html"> Sucursales</a>
+								</li>
+								<li> <a href="../Correopostal/CorreoPostal2.html"> Correo postal</a>
+								</li>
+							</ul>
+						</li>
+						<li><a href="../Servicios/Servicios2.html">Servicios</a>
+							<ul class="Drop2">
+								<li> <a href="../Productos/Productos.html"> Componentes secundarios</a>
+								</li>
+							</ul>
+						</li>
+						<li><a href="../Contactanos2/Contactanos2.html">Contactanos</a>
+						</li>
+						<li><a href="../Tienda/Tienda2.html">Tienda</a>
+						</li>
+						<li><a href="../GESTION/GESTIONDETRABAJADORES.HTML">Trabajadores</a></li>
+					</ul>
+					<div id="rgbar"></div>
+				</section>
+			</header>
     </div>
-  );
-}
+);}
+
+
 
 export default App;
