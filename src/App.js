2f4f62a93bdb72bd52caaffe5bfccb0b1d6169c5
Agustin LLados <agustinllados@gmail.com>
2021-10-07 21:03:51 -0300

    desafio clase 6

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar.jsx'; 
 import Main from './components/Main/Main.jsx';
 import Footer from './components/Footer/Footer.jsx';
+import Item from './components/Item/Item.jsx';
 
 
 function App() {
   return (
     <div className="App">
 		<NavBar/>
+		<Item/>
 		<Main/>
         <Footer/>
 	</div>
---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Item() {
+    const Item = [
+[
+    {
+      modelo: " B550M-DS3H-rev-1x",
+      marca: "Gigabyte",
+      precio: 10000,
+       },
+    { modelo: " Rx-580",
+      marca: "Asrock",
+      precio: 10000,
+    },
+
+    { modelo: "Ryzen 5 3600xt",
+      marca: "AMD",
+      precio: 20000,
+      }
+    ]
+];
+const getItem = new Promise((resolve, reject) => {
+    setTimeout(() =>resolve(Item),2000)
+});
+getItem.then((resolve) =>console.log(resolve));
+    return (
+        <div>
+            
+        </div>
+    )
+}
